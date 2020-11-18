370ac15d32e36772c03a40258585e195f9569de6
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2020-11-19 03:25:09 +0500

    add netlify identity

--- packages/base/src/pages/index.js
+++ packages/base/src/pages/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Heading, Button, Flex } from 'theme-ui';
+import netlifyIdentity from 'netlify-identity-widget';
 
-export default props => (
-    <Container>
-        <Flex sx={{ flexDirection: "column", padding: 3, textAlign: "center" }}>
-            <Heading as="h1">Todo List</Heading>
-            <Button sx={{ marginTop: 2, color: 'black' ,fontFamily:'monospace' ,cursor: 'pointer' }} onClick={() => { alert("Login Successfully!") }}>
-                LOGIN
-            </Button>
-        </Flex>
-    </Container>
-);
+export default props => {
+    useEffect( () =>{
+        netlifyIdentity.init({});
+    });
+    return(
+        <Container>
+            <Flex sx={{ flexDirection: "column", padding: 3, textAlign: "center" }}>
+                <Heading as="h1">Todo List</Heading>
+                <Button sx={{ marginTop: 2, color: 'black' ,fontFamily:'monospace' ,cursor: 'pointer' }} onClick={() => { netlifyIdentity.open() }}>
+                    LOGIN
+                </Button>
+            </Flex>
+        </Container>
+    )
+};
